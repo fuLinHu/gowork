a70e77c5708784c6c62907d1796bbb68bd0220c2
fulinhu <1205668006@qq.com>
2019-10-17 00:24:05 +0800

    第一次提交

---
+++ basetest/basetest.go
@@ -0,0 +1,32 @@
+package main
+
+/*func main() {
+	var a string = "Runoob"
+	fmt.Println(a)
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var bc = "fulinhu"
+	var bb = 1.0;
+	fmt.Println(bc)
+	fmt.Println(bb)
+}*/
+
+/*func main(){
+	i :=1
+	i,j:=1,3
+	fmt.Println(i+j)
+}*/
+var j int = 10
+var kk = 100
+var (
+	k int
+	b = 100
+)
+/*func main(){
+	//k :=0
+	println(j)
+	println(k)
+	_,kkk := 3,5
+	println(kkk)
+}*/
